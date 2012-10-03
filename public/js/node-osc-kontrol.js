ae0889d367719670148f2816b32805e1e453134a
Myles Borins <myles.borins@gmail.com>
2012-10-03 04:19:45 -0700

    X/Y Displaying

--- public/js/node-osc-kontrol.js
+++ public/js/node-osc-kontrol.js
@@ -1,7 +1,20 @@
-/*global jQuery, io*/
+/*global jQuery, io, console*/
 
-var socket = io.connect('http://basestation.local:1337');
+var oscControl = oscControl || {};
 
 (function ($) {
     'use strict';
+    oscControl.socket = io.connect('http://basestation.local:1337');
+    oscControl.pad = $(".pad");//
+    oscControl.pad.xy({
+        displayPrevious: false,
+        min: 0,
+        max: 127,
+        fgColor: "#222222",
+        bgColor: "#EEEEEE",
+        change: function (values) {
+            console.log("change : ", values);
+        }
+    });
+    oscControl.pad.css({'border': '5px solid #BBB'});
 }(jQuery));
